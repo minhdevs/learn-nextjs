c83728ec29bd3d172e62b2a368ad95247bb8f7bd
MWare <124526287+mwarevn@users.noreply.github.com>
2024-06-03 21:35:22 +0700

    Update

---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface IAppProps {}
+
+export default function App(props: IAppProps) {
+  return <div>Hello, this is admin page.</div>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function App() {
+  return <div>This is the dashboard page.</div>;
+}
